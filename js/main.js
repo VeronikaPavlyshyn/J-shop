89d22ec5fb78d71ff40329339aa4a5ac57cbe2b5
Veronika <pavlisinveronika@gmail.com>
2022-08-24 17:37:19 +0300

    add file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.testmonials__slider').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });})
+ 
